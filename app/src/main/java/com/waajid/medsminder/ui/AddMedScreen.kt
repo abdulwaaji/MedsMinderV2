63a6fe0fcd2b3b27430d63fff554de5f15787391
Abdul Waajid <abdul_waajid@reizendretail.in>
2023-09-18 09:03:21 +0530

    add taking input from textfield and saving to db

--- app/src/main/java/com/waajid/medsminder/ui/AddMedScreen.kt
+++ app/src/main/java/com/waajid/medsminder/ui/AddMedScreen.kt
@@ -1,17 +1,24 @@
 package com.waajid.medsminder.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.waajid.medsminder.model.MedViewModel
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.waajid.medsminder.model.Med
@@ -19,49 +26,56 @@ import com.waajid.medsminder.model.Med
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddMedsScreen() {
-    Column {
+    Column(modifier = Modifier.
+                        fillMaxWidth().
+                        padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
 
 
         val medViewModel: MedViewModel = hiltViewModel()
-        val medName = remember { mutableStateOf(TextFieldValue("")) }
+        var medName by remember { mutableStateOf(TextFieldValue("")) }
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
+//            modifier = Modifier.fillMaxWidth(),
             label = { Text(text = "Medicine Name") },
-            value = medName.value,
-            onValueChange = { medName.value = it })
+            value = medName,
+            onValueChange = { medName = it })
 
-        val ailmentName = remember { mutableStateOf(TextFieldValue("")) }
+        var ailmentName by remember { mutableStateOf(TextFieldValue("")) }
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier,
             label = { Text(text = "Ailment Name") },
-            value = ailmentName.value,
-            onValueChange = { ailmentName.value = it })
+            value = ailmentName,
+            onValueChange = { ailmentName = it })
 
-        val intakeQty = remember { mutableStateOf(TextFieldValue("")) }
+        var intakeQty by  remember { mutableStateOf(TextFieldValue("")) }
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier,
             label = { Text(text = "Intake Quantity") },
-            value = intakeQty.value,
-            onValueChange = { intakeQty.value = it })
+            value = intakeQty,
+            onValueChange = { intakeQty = it })
 
-        val availableStock = remember { mutableStateOf(TextFieldValue("")) }
+        var availableStock by remember { mutableStateOf(TextFieldValue("")) }
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            label = { Text(text = "Intake Quantity") },
-            value = availableStock.value,
-            onValueChange = { availableStock.value = it })
+            modifier = Modifier,
+            label = { Text(text = "Available Stock") },
+            value = availableStock,
+            onValueChange = { availableStock = it })
+
+        Spacer(modifier = Modifier.size(16.dp))
 
         Button(onClick = {
 
             val med = Med(
-                drugName = medName.value.toString(),
-                ailment = ailmentName.toString(),
-                conSummingQuantity = 0.5,//intakeQty.value.toString().toDouble(),
+                drugName = medName.text,
+                ailment = ailmentName.text,
+                conSummingQuantity = intakeQty.text.toDouble(),
                 drugUOM = "Tablet",
                 drugColorId = "Blue",
                 schedule = listOf("Every 4 hours"),
                 beforeAfterFood = "After food",
-                availableStock = 14.9//availableStock.value.toString().toDouble()
+                availableStock = availableStock.text.toDouble()
             )
 
             medViewModel.insertData(med)
@@ -71,16 +85,7 @@ fun AddMedsScreen() {
 
     }
 }
-/*
-*     @PrimaryKey(autoGenerate = true) var id: Int?= null,
-    var drugName: String,
-    var ailment: String,
-    var conSummingQuantity: Double,
-    val drugUOM: String,
-    var drugColorId: String,
-    var schedule: List<String>,
-    var beforeAfterFood: String,
-    var availableStock: Double*/
+
 
     @Preview
     @Composable
